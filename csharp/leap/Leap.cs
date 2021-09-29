767163cbca90658008658a27d9502e51e9bdb2b7
Carlos Villalba <cvillalba@dxc.com>
2021-09-29 11:05:39 +0200

    leap

---
+++ csharp/leap/Leap.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+        if (year % 400 == 0) return true;
+
+        if (year % 100 != 0)
+        {
+            return (year % 4 == 0);
+        }
+
+        return false;
+        
+    }
+}
