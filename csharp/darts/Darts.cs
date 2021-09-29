69c5e713ee31c47875b00d8824bbff035f82e614
Carlos Villalba <cvillalba@dxc.com>
2021-09-29 16:54:50 +0200

    Darts

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Darts
+{
+
+    public static double Radius(double x, double y) => Math.Sqrt(Math.Pow(x, 2d) + Math.Pow(y, 2d));
+
+    public static int Score(double x, double y)
+    {
+        double radius = Radius(x, y);
+
+        return radius switch
+        {
+            <= 1 => 10,
+            <= 5 => 5,
+            <= 10 => 1,
+            _ => 0
+        };
+    }
+}
