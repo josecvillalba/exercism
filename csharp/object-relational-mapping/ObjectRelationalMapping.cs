ad258f0e42b2566b63229f394b8a780ca9e8db47
Carlos Villalba <cvillalba@dxc.com>
2021-10-18 18:12:32 +0200

    Object Relational Mapping

---
+++ csharp/object-relational-mapping/ObjectRelationalMapping.cs
@@ -0,0 +1,46 @@
+using System;
+
+
+public class Orm: IDisposable
+{
+    private Database database;
+    
+    public Orm(Database database)
+    {
+        this.database = database;
+    }
+
+    public void Begin()
+    {
+        database.BeginTransaction();
+    }
+
+    public void Write(string data)
+    {
+        try
+        {
+            database.Write(data);
+        }
+        catch (Exception e)
+        {
+            database.Dispose();    
+        }
+    }
+
+    public void Commit()
+    {
+        try
+        {
+            database.EndTransaction();
+        }
+        catch (Exception e)
+        {
+            Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        database?.Dispose();
+    }
+}
