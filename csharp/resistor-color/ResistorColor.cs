7ac05e8e92ac2f99a82631ac46f3c5ee3c5c741e
Carlos Villalba <cvillalba@dxc.com>
2021-10-27 13:56:51 +0200

    Resistor Color

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color) => Array.IndexOf(Colors(), color);
+
+    public static string[] Colors() =>
+        new string[]
+        {
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white"
+        };
+}
